1146eee430ecc531d2ccd934e3f9cba42dafd757
locotive <locotive@naver.com>
2024-12-14 06:04:00 +0900

    Add configuration management for database and JWT settings
    - Introduced a new Config class in config.py to manage database and JWT configurations using environment variables.
    - Updated __init__.py to include the new Config class in the module's exports, enhancing the overall configuration structure.
    - This change improves maintainability and clarity in managing application settings.

--- app/config/__init__.py
+++ app/config/__init__.py
@@ -1,5 +1,12 @@
 from .location_config import LocationConfig
 from .job_config import JobConfig
 from .search_config import SearchConfig, SearchParams
+from .app_config import Config
 
-__all__ = ['LocationConfig', 'JobConfig', 'SearchConfig', 'SearchParams'] 
+__all__ = [
+    'LocationConfig', 
+    'JobConfig', 
+    'SearchConfig', 
+    'SearchParams',
+    'Config'
+] 
